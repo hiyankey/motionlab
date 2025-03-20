d2a13ac1bf9de413fcc122cf275d97e2d85859b6
Emmanuel Yankey <hiyankey@gmail.com>
2025-03-20 04:42:43 +0000

    use animate prop

--- app/(module)/02/page.tsx
+++ app/(module)/02/page.tsx
@@ -6,7 +6,12 @@ export default function Page() {
     <div className="flex h-screen place-items-center justify-center bg-gray-100">
       <motion.button
         className="rounded-lg bg-black px-3 py-1.5 text-white"
-        animate={{ backgroundColor: 'red' }}
+        animate={{
+          backgroundColor: '#000',
+          scale: 2,
+          y: '50%',
+          rotate: 18000,
+        }}
       >
         Get started
       </motion.button>
